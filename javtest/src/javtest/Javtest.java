3e7ce49e96db58ea8eb34ba3d22cebf24eacd1c6
dinhductai <dinhductai2501@gmail.com>
2024-05-06 16:17:26 +0800

    begin code

---
+++ javtest/src/javtest/Javtest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javtest;
+
+/**
+ *
+ * @author admin
+ */
+public class Javtest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hellooooo");
+    }
+    
+}
